e6bbd4eec3fcf5d41cdb8686ed70909e4e7d2611
Christian Peters <c.peters@makermaker.de>
2023-03-12 19:14:48 +0100

    basic architecture with user mdoel"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
+import './config/config'
 
 const app = express()
+const PORT = process.env.PORT
 
-app.listen(4545, () => console.log("running server"))
+app.listen(PORT, () => console.log("running", PORT))
