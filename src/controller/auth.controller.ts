2f8a887c61004d9c3f37e0c3312bdcd3509f11a0
Christian Peters <c.peters@makermaker.de>
2023-03-14 09:33:58 +0100

    save the date when user loged in

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import User from "../models/user.model";
 import { createToken } from "../util/jwt";
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: true
+}
 
 export const register = async (req: Request, res: Response) => {
     const newUser = new User(req.body)
@@ -20,8 +24,10 @@ export const login = async (req: Request, res: Response) => {
         if (user === null) {
             res.status(403).end()
         } else {
-            console.log(typeof user._id);
             const token = createToken({ id: user._id, role: user.role })
+            res.cookie('lmsshorttoken', token, cookieOptions)
+            user.lastLogin = new Date()
+            user.save()
             res.json(token)
         }
 
