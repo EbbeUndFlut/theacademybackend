b3d4eb13eb9197863601bbdec1604d5261667252
Christian Peters <c.peters@makermaker.de>
2023-03-16 14:30:50 +0100

    removed  unused import

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { login, register } from '../controller/auth.controller'
-import { checkJwt } from '../middleware/authorisationMiddleware'
 import { encrypt } from '../middleware/encryptMiddleware'
 
 export const router = express.Router()
